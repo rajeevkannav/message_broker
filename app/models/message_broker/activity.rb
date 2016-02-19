3313c17cddf84b007daf6f7d2490e9747089867e
Rajeev Kannav Sharma <rajeev@headerlabs.com>
2016-02-19 18:17:35 +0530

    UI fixes minor

--- app/models/message_broker/activity.rb
+++ app/models/message_broker/activity.rb
@@ -1,4 +1,6 @@
 module MessageBroker
   class Activity < ActiveRecord::Base
+    # Validations
+    validates_presence_of :name, :template_text
   end
 end
--- app/models/message_broker/rule.rb
+++ app/models/message_broker/rule.rb
@@ -1,5 +1,9 @@
 module MessageBroker
   class Rule < ActiveRecord::Base
+    # Validations
+    validates_presence_of :target, :event, :activity
+    # Associations
     belongs_to :activity
+
   end
 end
