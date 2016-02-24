8015c59916930bdb0782502eecf7f5fa9f497bca
Rajeev Kannav Sharma <rajeev@headerlabs.com>
2016-02-24 16:28:44 +0530

    Rule index UI CSS improved much

--- app/helpers/message_broker/application_helper.rb
+++ app/helpers/message_broker/application_helper.rb
@@ -1,6 +1,12 @@
 module MessageBroker
   module ApplicationHelper
 
+    def model_list
+      Rails.application.eager_load!
+      _list = (ActiveRecord::Base.subclasses.map { |subclass| subclass.to_s unless subclass.to_s.include? 'MessageBroker::' } - [ActiveRecord::SchemaMigration]).compact!
+      _list - ['ActiveRecord::SchemaMigration']
+    end
+
     def link_material_icon_to(link, icon_name, text = nil)
       link_to link do
         if text.nil?
