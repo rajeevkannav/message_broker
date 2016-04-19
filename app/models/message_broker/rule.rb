452256fd83eaebfce4f0e01e6914eef27c5e8dde
Rajeev Kannav Sharma <rajeev@headerlabs.com>
2016-04-19 13:25:48 +0530

    CALLBACK_DURATION Renamed. Task 1 completed, three pending.

--- app/models/message_broker/rule.rb
+++ app/models/message_broker/rule.rb
@@ -2,13 +2,13 @@ module MessageBroker
   class Rule < ActiveRecord::Base
 
     # constants
-    CALLBACK_DURATION = %w(before after)
+    CALLBACK_TYPE = %w(before after)
     LAPSE_MAGNITUDE = 0...99
     LAPSE_UNIT = %w(seconds hours days weeks fortnights months years)
 
     # Validations
     validates :target, :event, :activity, :callback_duration, :lapse_magnitude, :lapse_unit, presence: true
-    validates :callback_duration, inclusion: {in: MessageBroker::Rule::CALLBACK_DURATION, message: "value %{value} is not included in the list"}
+    validates :callback_duration, inclusion: {in: MessageBroker::Rule::CALLBACK_TYPE, message: "value %{value} is not included in the list"}
     validates :lapse_magnitude, inclusion: {in: MessageBroker::Rule::LAPSE_MAGNITUDE, message: "value %{value} is not included in the list"}
     validates :lapse_unit, inclusion: {in: MessageBroker::Rule::LAPSE_UNIT, message: "value %{value} is not included in the list"}
 
@@ -17,3 +17,11 @@ module MessageBroker
 
   end
 end
+
+### Todo: CALLBACK_DURATION Rename ??
+### Todo: Application Helper RE-write
+### Todo: instance_method_list should be validated.
+### Todo: Write How it works?
+
+
+
--- db/migrate/20160218145608_create_message_broker_rules.rb
+++ db/migrate/20160218145608_create_message_broker_rules.rb
@@ -4,7 +4,7 @@ class CreateMessageBrokerRules < ActiveRecord::Migration
       t.string :target
       t.string :event
       t.belongs_to :activity, index: true
-      t.string :callback_duration
+      t.string :callback_type
       t.integer :lapse_magnitude
       t.string :lapse_unit
 
